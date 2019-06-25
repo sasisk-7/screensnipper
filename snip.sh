387985aad731a6a92a6366b9482f3c302c9e6ab7
sasisk-7 <52200284+sasisk-7@users.noreply.github.com>
2019-06-25 16:55:18 +0530

    Update snip.sh

--- snip.sh
+++ snip.sh
@@ -2,5 +2,17 @@
 
 echo "enter the save name:"
 read name 
-import -quiet -monitor $a /$PWD/$name.png
-echo "Done"
+echo "what Do you want  1.Full screenshot 2.Snip the screen"
+read num
+
+
+
+
+if [ $num -eq 1 ]
+then
+  import -quiet -window root -monitor $a /$PWD/$name.png
+elif [ $num -eq 2 ]
+then
+import -quiet -monitor $a /$PWD/$name.png 
+echo " Done"
+fi
