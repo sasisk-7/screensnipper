1afdb32f21e28e265210ebb63902132e80958d49
sasisk-7 <52200284+sasisk-7@users.noreply.github.com>
2019-06-26 21:46:04 +0530

    Update snip.sh

--- snip.sh
+++ snip.sh
@@ -1,12 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
+dpkg -s imagemagick  &> /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Package  is installed!"
 echo "enter the save name:"
 read name 
 echo "what Do you want  1.Full screenshot 2.Snip the screen"
 read num
-
-
-
 
 if [ $num -eq 1 ]
 then
@@ -16,3 +17,13 @@ then
 import -quiet -monitor $a /$PWD/$name.png 
 echo " Done"
 fi
+
+else
+    echo "Package  is NOT installed!"
+    echo " Package credits :ImageMagic"
+     sudo apt-get --force-yes --yes install imagemagick 
+     chmod +x $PWD/snippkg.sh
+     ./$PWD/snippkg.sh
+fi
+ echo $?
+if 
